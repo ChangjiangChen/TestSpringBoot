a6a01e88c2033e3f29d04ec0cf3db57983663e95
changjiang <changjiang0018@126.com>
2018-07-05 15:26:37 +0800

    自定义错误页面--测试可用

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,11 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 //开启组建扫描和自动配置
-@SpringBootApplication(scanBasePackages = "com.example.demo.*")
+@SpringBootApplication(scanBasePackages = "com.example.demo.*", exclude = ErrorMvcAutoConfiguration.class)
 @MapperScan(basePackages = "com.example.demo.dao")
 @EnableTransactionManagement
 public class DemoApplication {
